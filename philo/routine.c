260a521f39162d96e2356150460c28d6f021e363
Leo Langlois <lelanglo@c2r10p4.42lehavre.fr>
2025-02-04 11:11:27 +0100

    juste peut etre un leger truc a regler

--- philo/routine.c
+++ philo/routine.c
@@ -6,44 +6,49 @@
 /*   By: lelanglo <lelanglo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 16:06:03 by lelanglo          #+#    #+#             */
-/*   Updated: 2025/02/04 10:27:03 by lelanglo         ###   ########.fr       */
+/*   Updated: 2025/02/04 11:04:20 by lelanglo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	unlock_fork(t_philosopher *philo)
+static void	unlock_fork(pthread_mutex_t *first_fork,
+	pthread_mutex_t *second_fork)
 {
-	pthread_mutex_unlock(philo->left_fork);
-	pthread_mutex_unlock(philo->right_fork);
+	pthread_mutex_unlock(first_fork);
+	pthread_mutex_unlock(second_fork);
 }
 
 static void	philo_eat(t_philosopher *philo)
 {
-	pthread_mutex_lock(philo->left_fork);
-	if (detect_death(philo))
+	pthread_mutex_t	*first_fork;
+	pthread_mutex_t	*second_fork;
+
+	if (philo->id % 2 == 0)
 	{
-		pthread_mutex_unlock(philo->left_fork);
-		return ;
+		first_fork = philo->left_fork;
+		second_fork = philo->right_fork;
 	}
+	else
+	{
+		first_fork = philo->right_fork;
+		second_fork = philo->left_fork;
+	}
+	pthread_mutex_lock(first_fork);
+	if (detect_death(philo))
+		return (pthread_mutex_unlock(first_fork), (void)1);
 	mutex_print(philo, "has taken a fork\n");
-	pthread_mutex_lock(philo->right_fork);
+	pthread_mutex_lock(second_fork);
 	if (detect_death(philo))
-	{
-		unlock_fork(philo);
-		return ;
-	}
+		return (unlock_fork(first_fork, second_fork));
 	mutex_print(philo, "has taken a fork\n");
 	mutex_print(philo, "is eating\n");
+	philo->last_meal = get_time_in_ms();
 	usleep(philo->data->time_to_eat * 1000);
 	if (detect_death(philo))
-	{
-		unlock_fork(philo);
-		return ;
-	}
-	philo->last_meal = get_time_in_ms();
+		return (unlock_fork(first_fork, second_fork));
 	philo->meals_eaten++;
-	unlock_fork(philo);
+	unlock_fork(first_fork, second_fork);
 }
 
 static void	one_philo(t_philosopher *philo)
